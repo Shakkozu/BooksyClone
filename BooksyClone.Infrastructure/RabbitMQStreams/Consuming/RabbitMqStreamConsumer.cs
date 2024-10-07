0d648f0e04a458ae1583546e93b5eb86347441e8
adam.kolodziejczyk <shakkozuu@gmail.com>
2024-10-07 21:09:46 +0200

    Add rabbitMQ stream consumer

---
+++ BooksyClone.Infrastructure/RabbitMQStreams/Consuming/RabbitMqStreamConsumer.cs
@@ -0,0 +1,100 @@
+﻿using RabbitMQ.Stream.Client.Reliable;
+using RabbitMQ.Stream.Client;
+using System.Text;
+using Microsoft.Extensions.Hosting;
+using System.Net;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace BooksyClone.Infrastructure.RabbitMQStreams.Consuming;
+
+public record RabbitMQStreamConsumerConfiguration(
+    string StreamName,
+    string Username,
+    string Password,
+    string ConsumerName,
+    string RabbitMQAddress = "127.0.0.1",
+    int RabbitMQPort = 5552,
+    int ConnectionRetries = 3,
+    bool ShouldCreateStreamIfNotExists = true,
+    TimeSpan ConnectionTimeout = default
+)
+{
+    public RabbitMQStreamConsumerConfiguration(string streamName, string address, int port, string consumerName)
+        : this(streamName, "guest", "guest", consumerName, address, port, 3, true, TimeSpan.FromSeconds(10)) { }
+}
+
+public abstract class RabbitMQStreamsConsumer<T>: IHostedService where T: class
+{
+    private readonly RabbitMQStreamConsumerConfiguration _config;
+    private Consumer? _consumer;
+    private StreamSystem? _streamSystem;
+    private int _consumed;
+
+    protected RabbitMQStreamsConsumer(RabbitMQStreamConsumerConfiguration config)
+    {
+        _config = config;
+    }
+
+    protected abstract Task HandleAsync(T message);
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _consumed = 0;
+        var streamSystemConfig = new StreamSystemConfig
+        {
+            UserName = _config.Username,
+            Password = _config.Password,
+            Endpoints = [new IPEndPoint(IPAddress.Parse(_config.RabbitMQAddress), _config.RabbitMQPort),]
+        };
+        _streamSystem = await StreamSystem.Create(streamSystemConfig);
+        if (!await _streamSystem.StreamExists(_config.StreamName))
+        {
+            await _streamSystem.CreateStream(new StreamSpec(_config.StreamName)
+            {
+                MaxLengthBytes = 5_000_000_000
+            });
+        }
+
+        var offset = await _streamSystem.QueryOffset(_config.ConsumerName, _config.StreamName).ConfigureAwait(false);
+        var consumerConfig = new ConsumerConfig(_streamSystem, _config.StreamName)
+        {
+            Reference = _config.ConsumerName,
+            OffsetSpec = new OffsetTypeOffset(offset),
+            MessageHandler = async (stream, consumer, messageContext, message) =>
+            {
+                var messageStringContent = Encoding.UTF8.GetString(message.Data.Contents);
+                var serializedMessage = JsonConvert.DeserializeObject<T>(messageStringContent);
+                if(serializedMessage == null)
+                    throw new InvalidCastException($"Deserialization of message {messageStringContent} failed casting to an object. Stream: {stream}, Index: {messageContext.Offset}, Timestamp: {messageContext.Timestamp}");
+
+                try
+                {
+                    await HandleAsync(serializedMessage).ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    Log.Error("Processing received message failed with error: {@error}. Stream: {stream}, offset: {offset}", e, stream, messageContext.Offset);
+                }
+                if(_consumed++ % 1000 == 0)
+                    await consumer.StoreOffset(messageContext.Offset).ConfigureAwait(false);
+
+            }
+        };
+        Log.Information("Starting consumer for stream {StreamName}", _config.StreamName);
+        _consumer = await Consumer.Create(consumerConfig);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        if(_consumer != null)
+        {
+            await _consumer.Close().ConfigureAwait(false);
+        }
+        if(_streamSystem != null)
+        {
+            await _streamSystem.Close().ConfigureAwait(false);
+
+        }
+    }
+}
--- BooksyClone.Tests/Infrastructure/RabbitMQStreamsIntegrationTests.cs
+++ BooksyClone.Tests/Infrastructure/RabbitMQStreamsIntegrationTests.cs
@@ -1,4 +1,6 @@
-﻿using BooksyClone.Infrastructure.RabbitMQStreams.Producing;
+﻿using BooksyClone.Infrastructure.RabbitMQStreams.Consuming;
+using BooksyClone.Infrastructure.RabbitMQStreams.Producing;
+using FakeItEasy;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BooksyClone.Tests.Infrastructure;
@@ -6,20 +8,33 @@ namespace BooksyClone.Tests.Infrastructure;
 internal class RabbitMQStreamsIntegrationTests
 {
     private TestProducer _testProducer;
+    private TestConsumer _testConsumer;
+    private ITestService _fakeDependency;
     private BooksyCloneApp _app;
 
     private RabbitMQStreamProducerConfiguration GetTestConfiguration => new RabbitMQStreamProducerConfiguration(
         "booksy-clone-integration-tests-stream",
         "127.0.0.1",
         5552);
+    private RabbitMQStreamConsumerConfiguration GetTestConsumerConfiguration => new RabbitMQStreamConsumerConfiguration(
+        "booksy-clone-integration-tests-stream",
+        "127.0.0.1",
+        5552,
+        "test-consumer");
 
     [SetUp]
     public void Setup()
     {
         _testProducer = new TestProducer(GetTestConfiguration);
+        _fakeDependency = A.Fake<ITestService>();
+        _testConsumer = new TestConsumer(_fakeDependency, GetTestConsumerConfiguration);
         _app = BooksyCloneApp.CreateInstance(services =>
         {
             services.AddSingleton(_testProducer);
+            services.AddHostedService(sp =>
+            {
+                return _testConsumer;
+            });
         });
     }
 
@@ -30,16 +45,46 @@ internal class RabbitMQStreamsIntegrationTests
     }
 
     [Test]
-    public void PublishingShouldNotThrowAnyErrors()
+    public async Task ConsumerShouldConsumeReceivedMessages()
     {
-        Assert.DoesNotThrowAsync(async() => await _testProducer.Send(new TestMessage("Dupa", DateTime.Now)));
-    }
-    private record TestMessage(string Message, DateTime Timestamp);
+        var payload = new TestRabbitMqStreamsMessage("Dupa", DateTime.Now);
+        await _testProducer.Send(payload);
 
-    private class TestProducer : RabbitMqStreamProducer<TestMessage>
+        Task.Delay(50).Wait();
+
+        A.CallTo(() => _fakeDependency.Call(A<TestRabbitMqStreamsMessage>.That.IsEqualTo(payload)))
+            .MustHaveHappenedOnceExactly();
+    }
+
+    private class TestProducer : RabbitMqStreamProducer<TestRabbitMqStreamsMessage>
     {
         public TestProducer(RabbitMQStreamProducerConfiguration config) : base(config)
         {
         }
     }
 }
+public record TestRabbitMqStreamsMessage(string Message, DateTime Timestamp);
+
+internal class TestConsumer : RabbitMQStreamsConsumer<TestRabbitMqStreamsMessage>
+{
+    private readonly ITestService _testService;
+    private RabbitMQStreamConsumerConfiguration _config;
+
+    public TestConsumer(ITestService testService, RabbitMQStreamConsumerConfiguration config) : base(config)
+    {
+        _testService = testService;
+        _config = config;
+    }
+
+    protected override Task HandleAsync(TestRabbitMqStreamsMessage message)
+    {
+        _testService.Call(message);
+        return Task.CompletedTask;
+    }
+}
+
+public interface ITestService
+{
+    void Call(TestRabbitMqStreamsMessage message);
+}
+
