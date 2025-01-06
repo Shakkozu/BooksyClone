839e46356ad951fbe40004259c86689bc9674699
adam.kolodziejczyk <shakkozuu@gmail.com>
2025-01-06 08:14:25 +0100

    Configure migrations runner to keep modules data in separated schemas, and to run migrations for each module in its dedicated version table

---
+++ BooksyClone.Search.Migrations/0001_AddServiceToTimeRequiredTable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace BooksyClone.Infrastructure.Migrations.Search;
+
+
+[Migration(0001, "Add service type to time required table")]
+public class _0001_AddServiceToTimeRequiredTable : Migration
+{
+	public override void Up()
+	{
+		Create.Table("test")
+			.InSchema("search")
+			.WithColumn("id").AsInt64().PrimaryKey().Identity()
+			.WithColumn("guid").AsGuid().NotNullable().Unique().WithDefaultValue(SystemMethods.NewSequentialId);
+	}
+
+	public override void Down()
+	{
+		Delete.Table("test");
+	}
+}
